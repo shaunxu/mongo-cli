466b50151a46290c47330dc9c62a35b45880417f
Shaun Xu <jfarrio@gmail.com>
2016-07-27 14:08:08 +0800

    .

--- index.js
+++ index.js
@@ -8,30 +8,24 @@ const user = argv.user;
 const pass = argv.pass;
 const authDb = argv['auth-db'];
 
-const command = require('./command');
-if (command && command.collection && command.query) {
-    const MongoClient = require('mongodb').MongoClient;
-    const uri = `mongodb://${host}:${port}/${authDb}`;
-    MongoClient.connect(uri, {
-        user: user,
-        pass: pass
-    }, (error, db) => {
-        if (error) {
-            console.log(`Error: Failed to connect to MongoDB: ${uri}. ${JSON.stringify(error, null, 2)}`);
-        }
-        else {
-            console.log(`INFO: Connected to MongoDB ${uri}`);
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb://${host}:${port}/${authDb}`;
+MongoClient.connect(uri, {
+    user: user,
+    pass: pass
+}, (error, db) => {
+    if (error) {
+        console.log(`Error: Failed to connect to MongoDB: ${uri}. ${JSON.stringify(error, null, 2)}`);
+    }
+    else {
+        console.log(`INFO: Connected to MongoDB ${uri}`);
 
-            require('./command')(db, (error) => {
-                if (error) {
-                    console.log(`Error: Failed to run command on MongoDB. ${JSON.stringify(error, null, 2)}`);
-                }
-                db.close();
-                console.log(`Bye.`);
-            });
-        }
-    });
-}
-else {
-    console.log(`Error: We need query.`);
-}
+        require('./command')(db, (error) => {
+            if (error) {
+                console.log(`Error: Failed to run command on MongoDB. ${JSON.stringify(error, null, 2)}`);
+            }
+            db.close();
+            console.log(`Bye.`);
+        });
+    }
+});
